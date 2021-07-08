1a9fe328d55b20313ccbf171f79de0b789700926
Cleiton Oliveira <idd_caraujo@uolinc.com>
2021-07-07 22:09:02 -0300

    DAY TWO

--- src/main/java/com/zherro/controledeestoque/controller/CategoriaController.java
+++ src/main/java/com/zherro/controledeestoque/controller/CategoriaController.java
@@ -6,8 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/categorias")
@@ -24,4 +28,39 @@ public class CategoriaController
         model.addAttribute("categorias", categorias);
         return "crud/categoria/categoria.html";
     }
+
+    @GetMapping("/form-categoria")
+    public String formCategoria( Model model ) {
+        Categoria categoria = new Categoria();
+
+        model.addAttribute("categoria", categoria);
+        model.addAttribute("emEdicao", false);
+
+        return "crud/categoria/form-categoria.html";
+    }
+
+    @GetMapping("/editar-categoria")
+    public String editarCategoria( @RequestParam Long idCategoria, Model model) {
+        Categoria categoria = categoriaService.buscaPorId(idCategoria);
+
+        model.addAttribute("categoria", categoria);
+        model.addAttribute("emEdicao", true);
+
+        return "crud/categoria/form-categoria.html";
+    }
+
+    @PostMapping("/salvar-categorias")
+    public String criarCategoria(Categoria categoria, Model model) {
+        categoria.setStatus(true);
+        categoriaService.salvar(categoria);
+        return "redirect:/categorias";
+    }
+
+    @PostMapping("/remover-categoria")
+    public String removerCategoria( @RequestParam Long idCategoria, Model model) {
+        categoriaService.delete(idCategoria);
+        return "redirect:/categorias";
+    }
+
+
 }
--- src/main/java/com/zherro/controledeestoque/repository/CategoriaRepository.java
+++ src/main/java/com/zherro/controledeestoque/repository/CategoriaRepository.java
@@ -1,10 +1,16 @@
 package com.zherro.controledeestoque.repository;
 
 import com.zherro.controledeestoque.domain.Categoria;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>
 {
+
+     @Query(value = "select * from  tb_categorias c WHERE c.status ORDER BY c.id DESC", nativeQuery = true)
+//    @Query("FROM Categoria c WHERE c.status = true ORDER BY c.id DESC")
+    List<Categoria> findAll();
 }
